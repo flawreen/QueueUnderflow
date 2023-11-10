1313e252a5540bb2f417d23c97920e3e744bcc73
Florin Craciun <83332450+flawreen@users.noreply.github.com>
2023-11-10 15:40:00 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QueueUnderflow.Models;
+
+namespace QueueUnderflow.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QueueUnderflow.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Continutul este obligatoriu")]
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public int TopicId { get; set; }
+        public virtual Topic Topic { get; set; }
+    }
+}
