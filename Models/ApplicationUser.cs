0a07f7b84291fe6d0c6283689eacadfa927e333a
Nazare Elena Denisa <nazareelena188@gmail.com>
2024-01-13 22:01:55 +0200

    Completed required tasks

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -6,14 +6,10 @@ namespace QueueUnderflow.Models
 {
     public class ApplicationUser : IdentityUser
     { 
-        // un user poate posta mai multe raspunsuri
         public virtual ICollection<Answer>? Answers { get; set; }
 
-        // un user poate posta mai multe discutii
         public virtual ICollection<Discussion>? Discussions { get; set; }
 
-
-        // se pot adauga si atribute suplimentare
         public string? FirstName { get; set; }
 
         public string? LastName { get; set; }
